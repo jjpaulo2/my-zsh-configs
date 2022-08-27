f3d9a1c9d9464a68747da7163f083d50a6468980
João Paulo Carvalho <jjpaulo2@protonmail.com>
2022-08-27 17:39:19 -0300

    pop-os: testing many commands

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env zsh
 # -*- coding: utf-8 -*-
 
-# Install Oh-My-Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+install_oh_my_zsh() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+}
 
-# Install asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+install_asdf() {
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+}
 
-# Install Zplug
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+test_echo() {
+    echo "pombs"
+}
 
-# Installing plugins with Zplug
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
-zplug "zsh-users/zsh-autosuggestions"
-zplug "MichaelAquilina/zsh-autoswitch-virtualenv"
+install_zplug_plugins() {
+    # Install Zplug
+    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+    # Installing plugins with Zplug
+    zplug "zsh-users/zsh-syntax-highlighting", defer:2
+    zplug "zsh-users/zsh-autosuggestions"
+    zplug "MichaelAquilina/zsh-autoswitch-virtualenv"
+}
+
+
+main() {
+    install_oh_my_zsh && test_echo
+}
+
+main
