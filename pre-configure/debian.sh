2796b261a9b6aa4ab00319c6cbb6819806d68eb9
João Paulo Carvalho <jjpaulo2@protonmail.com>
2022-08-27 19:08:59 -0300

    adding ; in scripts

--- pre-configure/debian.sh
+++ pre-configure/debian.sh
@@ -2,22 +2,20 @@
 # -*- coding: utf-8 -*-
 
 install_dependencies() {
-    apt update -y
-    apt install git zsh curl -y
+    apt update -y;
+    apt install git zsh curl -y;
 }
 
 install_oh_my_zsh() {
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" 
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 }
 
 main() {
 
     if install_dependencies; then
-        if install_oh_my_zsh; then
-            echo "You are ready to install ZSH configurations!" 
-        fi
+        install_oh_my_zsh;
     fi
     
 }
 
-main
+main;
